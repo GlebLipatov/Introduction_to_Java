117055cd01ee3f6eabab2f15c3606098a1ee9eb3
Gleb Lipatov <it.lipatov@gmail.com>
2023-06-07 12:30:46 +0300

    Task8 is done

--- src/Introduction/HomeWorks/HW1/Main.java
+++ src/Introduction/HomeWorks/HW1/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Task2 task2 = new Task2();
         Task3 task3 = new Task3();
         Task5 task5 = new Task5();
+        Task8 task8 = new Task8();
 
         task1.solution(100);
         task2.solution(1000);
         task3.start();
 
         task5.solution();
+        task8.solution();
     }
 }
---
+++ src/Introduction/HomeWorks/HW1/Task8.java
@@ -0,0 +1,34 @@
+package Introduction.HomeWorks.HW1;
+
+public class Task8 {
+    String message = "8. Задан массив, например, nums = [1,7,3,6,5,6]. \n" +
+            "Написать программу, которая найдет индекс i для массива\n" +
+            "такой, что сумма элементов с индексами < i равна сумме\n" +
+            "элементов с индексами > i.";
+
+    public void solution() {
+        System.out.println("\n" + message);
+
+        int[] nums = new int[]{1, 7, 3, 6, 5, 6};
+        int leftSum = 0;
+        int rightSum = 0;
+        int result = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            rightSum += nums[i];
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            leftSum += nums[i - 1];
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                result = i;
+                break;
+            }
+        }
+
+        if (result > 0) System.out.println("i = "+ result);
+        else System.out.println("There is no such i.");
+
+    }
+}
