8c16ece73f88cf6fc49a378206f4fa2c931e5118
Gleb Lipatov <it.lipatov@gmail.com>
2023-06-25 22:19:12 +0300

    hw5 task3 is done fix

--- src/Introduction/HomeWorks/HW5/Main.java
+++ src/Introduction/HomeWorks/HW5/Main.java
@@ -4,16 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-//        Task1 task1 = new Task1();
+        Task1 task1 = new Task1();
         Task3 task3 = new Task3();
 
-//        task1.solution();
-//        Task2.solution();
+        task1.solution();
+        Task2.solution();
 
         ArrayList<Boolean[][]> fields = task3.solution();
-
-        System.out.println(fields.size());
-
         for (var item : fields) {
             task3.print(item);
         }
